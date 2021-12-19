3d6b5407ad87d50c9b6aee8c958f25012b331c82
andyyu8588 <andy.yu8588@gmail.com>
2021-12-18 22:14:03 -0500

    offset -> index

--- src/debug.h
+++ src/debug.h
@@ -4,4 +4,4 @@
 #include "chunk.h"
 
 void disassembleChunk(Chunk& chunk, std::string name);
-size_t disassembleInstruction(Chunk& chunk, size_t offset);
+size_t disassembleInstruction(Chunk& chunk, size_t index);
