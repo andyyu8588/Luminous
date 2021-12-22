f77d1a5d71d9147f5acb0b928d6ed7c35d68f649
andyyu8588 <andy.yu8588@gmail.com>
2021-12-22 10:33:26 -0500

    fix format
    Co-authored-by: yunze1012 <yunze1012@users.noreply.github.com>

--- src/scanner.cpp
+++ src/scanner.cpp
@@ -1,229 +1,221 @@
-#include <iostream>
-#include <map>
-#include <exception>
 #include "scanner.h"
 
-Scanner::Scanner(std::string code) {
-    this->code = code;
-}
+#include <exception>
+#include <iostream>
+#include <map>
+
+Scanner::Scanner(std::string code) { this->code = code; }
 
 void Scanner::error(int line, std::string message) {
-    std::cerr << "ERROR: " << message << " (line " << line << ")." << std::endl;
-    errorOccured = true;
+  std::cerr << "ERROR: " << message << " (line " << line << ")." << std::endl;
+  errorOccured = true;
 }
 
-bool Scanner::isAtEnd() {
-    return (unsigned int)current >= code.length();
-}
+bool Scanner::isAtEnd() { return (unsigned int)current >= code.length(); }
 
-bool Scanner::isNumber(char c) {
-    return c >= '0' && c <= '9';
-}
+bool Scanner::isNumber(char c) { return c >= '0' && c <= '9'; }
 
 bool Scanner::isAlphabet(char c) {
-    return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '_';
+  return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '_';
 }
 
 char Scanner::nextChar() {
-    current++;
-    return code.at(current - 1);
+  current++;
+  return code.at(current - 1);
 }
 
 void Scanner::addToken(TokenType type) {
-    std::string lexeme = code.substr(start, current - start);
-    tokens.push_back(std::make_shared<Token>(type, lexeme, line));
+  std::string lexeme = code.substr(start, current - start);
+  tokens.push_back(std::make_shared<Token>(type, lexeme, line));
 }
 
 void Scanner::addToken(TokenType type, std::string lexeme) {
-    tokens.push_back(std::make_shared<Token>(type, lexeme, line));
+  tokens.push_back(std::make_shared<Token>(type, lexeme, line));
 }
 
 bool Scanner::match(char expected) {
-    if(isAtEnd() || code.at(current) != expected) return false;
+  if (isAtEnd() || code.at(current) != expected) return false;
 
-    current++;
-    return true;
+  current++;
+  return true;
 }
 
 char Scanner::peek() {
-    if(isAtEnd()) return '\0';
-    return code.at(current);
+  if (isAtEnd()) return '\0';
+  return code.at(current);
 }
 
 char Scanner::lookAhead() {
-    if((unsigned int)current + 1 >= code.length()) return '\0';
-    return code.at(current + 1);
+  if ((unsigned int)current + 1 >= code.length()) return '\0';
+  return code.at(current + 1);
 }
 
 void Scanner::string() {
-    // getting string
-    while(peek() != '"' && !isAtEnd()) {
-        if(peek() == '\n') {
-            line++;
-        }
-        nextChar();
+  // getting string
+  while (peek() != '"' && !isAtEnd()) {
+    if (peek() == '\n') {
+      line++;
     }
-
-    if(isAtEnd()) {
-        error(line, "Missing end of string");
-        return;
-    }
-
-    // remove closing quote symbol
     nextChar();
+  }
 
-    std::string str = code.substr(start + 1, current - 1 - (start + 1));
-    addToken(TokenType::STRING, str);
+  if (isAtEnd()) {
+    error(line, "Missing end of string");
+    return;
+  }
+
+  // remove closing quote symbol
+  nextChar();
+
+  std::string str = code.substr(start + 1, current - 1 - (start + 1));
+  addToken(TokenType::STRING, str);
 }
 
 void Scanner::number() {
-    // getting integer
-    while(isNumber(peek())) {
-        nextChar();
-    }
+  // getting integer
+  while (isNumber(peek())) {
+    nextChar();
+  }
 
-    // Handling floating point numbers
-    if(peek() == '.' && isNumber(lookAhead())) {
-        // consume '.'
-        nextChar();
-        while(isNumber(peek())) {
-            nextChar();
-        }
+  // Handling floating point numbers
+  if (peek() == '.' && isNumber(lookAhead())) {
+    // consume '.'
+    nextChar();
+    while (isNumber(peek())) {
+      nextChar();
     }
+  }
 
-    // case where variable name starts with a number
-    if(isAlphabet(peek())) {
-        error(line, "Variable names cannot start with a number");
-    }
+  // case where variable name starts with a number
+  if (isAlphabet(peek())) {
+    error(line, "Variable names cannot start with a number");
+  }
 
-    addToken(TokenType::NUM, code.substr(start, current - start));
+  addToken(TokenType::NUM, code.substr(start, current - start));
 }
 
 void Scanner::id() {
-    char curChar = peek();
-    // checking for id
-    while(isNumber(curChar) || isAlphabet(curChar)) {
-        nextChar();
-        curChar = peek();
-    }
+  char curChar = peek();
+  // checking for id
+  while (isNumber(curChar) || isAlphabet(curChar)) {
+    nextChar();
+    curChar = peek();
+  }
 
-    std::string id = code.substr(start, current - start);
-    auto typeIter = keywords.find(id);
-    if(typeIter != keywords.end()) {
-        addToken(typeIter->second);
-    }
-    else {
-        addToken(TokenType::ID);
-    }
+  std::string id = code.substr(start, current - start);
+  auto typeIter = keywords.find(id);
+  if (typeIter != keywords.end()) {
+    addToken(typeIter->second);
+  } else {
+    addToken(TokenType::ID);
+  }
 }
 
 void Scanner::scanToken() {
-    char c = nextChar();
-    switch (c) {
-        case '(':
-            addToken(TokenType::LPAREN);
-            break;
-        case ')':
-            addToken(TokenType::RPAREN);
-            break;
-        case '{':
-            addToken(TokenType::LBRACE);
-            break;
-        case '}':
-            addToken(TokenType::RBRACE);
-            break;
-        case '[':
-            addToken(TokenType::LBRACK);
-            break;
-        case ']':
-            addToken(TokenType::RBRACK);
-            break;
-        case '=':
-            addToken(TokenType::BECOMES);
-            break;
-        case '.':
-            addToken(TokenType::DOT);
-            break;
-        case '-':
-            addToken(TokenType::MINUS);
-            break;
-        case '+':
-            addToken(TokenType::PLUS);
-            break;
-        case ',':
-            addToken(TokenType::COMMA);
-            break;
-        case '*':
-            addToken(TokenType::STAR);
-            break;
-        case ';':
-            addToken(TokenType::SEMI);
-            break;
-        case '<':
-            if(match('=')) {
-                addToken(TokenType::LE);
-            }
-            else {
-                addToken(TokenType::LT);
-            }
-            break;
-        case '>':
-            if(match('=')) {
-                addToken(TokenType::GE);
-            }
-            else {
-                addToken(TokenType::GT);
-            }
-            break;
-        case '/':
-            // Handling single line comments:
-            if(match('/')) {
-                while(peek() != '\n' && !isAtEnd()) {
-                    nextChar();
-                }
-            }
-            else {
-                addToken(TokenType::SLASH);
-            }
-            break;
-        // useless characters filters:
-        case ' ': break;
-        case '\r': break;
-        case '\t': break;
-        // newline:
-        case '\n':
-            line++;
-            break;
-        // strings:
-        case '"':
-            string();
-            break;
-        default:
-            if(isNumber(c)) {
-                // could be a number
-                number();
-            }
-            else if(isAlphabet(c)) {
-                // could be an id or keyword
-                id();
-            }
-            else {
-                std::string message = "Unexpected character \'";
-                message.push_back(c);
-                message += "\'";
-                error(line, message);
-                break;
-            }
-    }
+  char c = nextChar();
+  switch (c) {
+    case '(':
+      addToken(TokenType::LPAREN);
+      break;
+    case ')':
+      addToken(TokenType::RPAREN);
+      break;
+    case '{':
+      addToken(TokenType::LBRACE);
+      break;
+    case '}':
+      addToken(TokenType::RBRACE);
+      break;
+    case '[':
+      addToken(TokenType::LBRACK);
+      break;
+    case ']':
+      addToken(TokenType::RBRACK);
+      break;
+    case '=':
+      addToken(TokenType::BECOMES);
+      break;
+    case '.':
+      addToken(TokenType::DOT);
+      break;
+    case '-':
+      addToken(TokenType::MINUS);
+      break;
+    case '+':
+      addToken(TokenType::PLUS);
+      break;
+    case ',':
+      addToken(TokenType::COMMA);
+      break;
+    case '*':
+      addToken(TokenType::STAR);
+      break;
+    case ';':
+      addToken(TokenType::SEMI);
+      break;
+    case '<':
+      if (match('=')) {
+        addToken(TokenType::LE);
+      } else {
+        addToken(TokenType::LT);
+      }
+      break;
+    case '>':
+      if (match('=')) {
+        addToken(TokenType::GE);
+      } else {
+        addToken(TokenType::GT);
+      }
+      break;
+    case '/':
+      // Handling single line comments:
+      if (match('/')) {
+        while (peek() != '\n' && !isAtEnd()) {
+          nextChar();
+        }
+      } else {
+        addToken(TokenType::SLASH);
+      }
+      break;
+    // useless characters filters:
+    case ' ':
+      break;
+    case '\r':
+      break;
+    case '\t':
+      break;
+    // newline:
+    case '\n':
+      line++;
+      break;
+    // strings:
+    case '"':
+      string();
+      break;
+    default:
+      if (isNumber(c)) {
+        // could be a number
+        number();
+      } else if (isAlphabet(c)) {
+        // could be an id or keyword
+        id();
+      } else {
+        std::string message = "Unexpected character \'";
+        message.push_back(c);
+        message += "\'";
+        error(line, message);
+        break;
+      }
+  }
 }
 
 std::vector<std::shared_ptr<Token>> Scanner::tokenize() {
-    // this loop scans token by token
-    while(!isAtEnd()) {
-        // next lexeme
-        start = current;
-        scanToken();
-    }
+  // this loop scans token by token
+  while (!isAtEnd()) {
+    // next lexeme
+    start = current;
+    scanToken();
+  }
 
-    return tokens;
+  return tokens;
 }
--- src/scanner.h
+++ src/scanner.h
@@ -1,79 +1,75 @@
 /*
-This is the scanner header file used to tokenize Luminous code. 
+This is the scanner header file used to tokenize Luminous code.
 Created by Yun Ze Zhou and Andy Yu.
 */
 
 #pragma once
-#include <string>
-#include <vector>
 #include <memory>
+#include <string>
 #include <unordered_map>
+#include <vector>
+
 #include "token.h"
 
-
 class Scanner {
+  std::string code;
+  std::vector<std::shared_ptr<Token>> tokens;
+  std::unordered_map<std::string, TokenType> keywords = {
+      {"equals", TokenType::EQ},   {"and", TokenType::AND},
+      {"or", TokenType::OR},       {"not", TokenType::NOT},
+      {"if", TokenType::IF},       {"else", TokenType::ELSE},
+      {"while", TokenType::WHILE}, {"return", TokenType::RETURN},
+      {"print", TokenType::PRINT}, {"addr", TokenType::ADDR},
+      {"at", TokenType::AT}};
+  int start = 0;
+  int current = 0;
+  int line = 1;
+  bool errorOccured = false;
 
-    std::string code;
-    std::vector<std::shared_ptr<Token>> tokens;
-    std::unordered_map<std::string, TokenType> keywords = {
-        {"equals", TokenType::EQ},
-        {"and", TokenType::AND},
-        {"or", TokenType::OR},
-        {"not", TokenType::NOT},
-        {"if", TokenType::IF},
-        {"else", TokenType::ELSE},
-        {"while", TokenType::WHILE},
-        {"return", TokenType::RETURN},
-        {"print", TokenType::PRINT},
-        {"addr", TokenType::ADDR},
-        {"at", TokenType::AT}
-    };
-    int start = 0;
-    int current = 0;
-    int line = 1;
-    bool errorOccured = false;
+  // return true if the scanner reached the end of file
+  bool isAtEnd();
 
-    // return true if the scanner reached the end of file
-    bool isAtEnd();
+  // return true if c is a number
+  bool isNumber(char c);
 
-    // return true if c is a number
-    bool isNumber(char c);
+  // return true if c is an alphabet or underscore
+  bool isAlphabet(char c);
 
-    // return true if c is an alphabet or underscore
-    bool isAlphabet(char c);
+  // scan single character token
+  void scanToken();
 
-    // scan single character token
-    void scanToken();
+  // feed in next character in source code
+  char nextChar();
 
-    // feed in next character in source code
-    char nextChar();
+  void addToken(TokenType type);
 
-    void addToken(TokenType type);
+  void addToken(TokenType type, std::string lexeme);
 
-    void addToken(TokenType type, std::string lexeme);
+  // consume current character and return true if current character is the
+  // character we expect
+  bool match(char expected);
 
-    // consume current character and return true if current character is the character we expect
-    bool match(char expected);
+  // peek the current character in source code (this does not advance the
+  // counter)
+  char peek();
 
-    // peek the current character in source code (this does not advance the counter)
-    char peek();
+  // peek the next characer in the source code (this does not advance the
+  // counter)
+  char lookAhead();
 
-    // peek the next characer in the source code (this does not advance the counter)
-    char lookAhead();
+  // string handler
+  void string();
 
-    // string handler
-    void string();
+  // number handler
+  void number();
 
-    // number handler
-    void number();
+  // identifier and keyword handler
+  void id();
 
-    // identifier and keyword handler
-    void id();
+  // error handler:
+  void error(int line, std::string message);
 
-    // error handler:
-    void error(int line, std::string message);
-
-public:
-    Scanner(std::string code);
-    std::vector<std::shared_ptr<Token>> tokenize();
+ public:
+  Scanner(std::string code);
+  std::vector<std::shared_ptr<Token>> tokenize();
 };
